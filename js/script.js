4918cbe48f4c120a65da7173f14e38695fde5a48
Eric Lahoda <elahoda@gmail.com>
2023-02-24 14:24:55 -0500

    JS with button show and disappear and color change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 button.addEventListener("click",function(){
-    cat.classList.add("show");
+        
+    if (cat.classList.contains("show")) {
+        cat.classList.remove("show");
+        button.innerText = "Wait Come back";
+        button.classList.add("disappear");
+        //    console.log("yes");
+      } else {
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat";
+        button.classList.remove("disappear");
+        //   console.log("no");
+      }
+
 });
